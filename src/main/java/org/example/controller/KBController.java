d425b297d96b2ebc91a44927c39590f27d3feb34
Hactually Benji <mrbenamiller@gmail.com>
2023-03-22 12:26:35 -0700

    Add dao and controller

--- src/main/java/org/example/controller/KBController.java
+++ src/main/java/org/example/controller/KBController.java
@@ -1,4 +1,25 @@
 package org.example.controller;
 
+import org.example.data.ArticleDBDao;
+import org.example.models.Article;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class KBController {
+    private final ArticleDBDao articleDAO;
+
+    public KBController(ArticleDBDao articleDAO) {
+        this.articleDAO = articleDAO;
+    }
+
+    @GetMapping
+    public void add() {
+        articleDAO.add();
+    }
 }
--- src/main/java/org/example/data/ArticleDBDao.java
+++ src/main/java/org/example/data/ArticleDBDao.java
@@ -1,4 +1,38 @@
 package org.example.data;
 
+import org.example.models.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
 public class ArticleDBDao {
+
+    @Autowired
+    private final JdbcTemplate jdbcTemplate;
+
+    public ArticleDBDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add() {
+        System.out.println(jdbcTemplate.query("SELECT * FROM Articles;", new ArticleMapper()));
+    }
+
+    private static final class ArticleMapper implements RowMapper<Article> {
+        @Override
+        public Article mapRow(ResultSet rs, int index) throws SQLException {
+            Article article = new Article();
+            article.setArticleID(rs.getInt("articleID"));
+            article.setArticleSubject(rs.getString("articleSubject"));
+            article.setArticleBody(rs.getString("articleBody"));
+            article.setAuthorizedRole(rs.getString("authorizedRole"));
+            article.setComplete(rs.getBoolean("isComplete"));
+            return article;
+        }
+    }
 }
