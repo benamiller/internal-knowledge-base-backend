1fbb999c22cec9d010148c59c6e296f259dcc2f0
Hactually Benji <mrbenamiller@gmail.com>
2023-03-22 10:58:13 -0700

    Define DTO objects for articles and comments

--- src/main/java/org/example/models/Article.java
+++ src/main/java/org/example/models/Article.java
@@ -1,4 +1,9 @@
 package org.example.models;
 
 public class Article {
+    private int articleID;
+    private String articleSubject;
+    private String articleBody;
+    private String authorizedRole;
+    private boolean isComplete;
 }
--- src/main/java/org/example/models/Comment.java
+++ src/main/java/org/example/models/Comment.java
@@ -1,4 +1,8 @@
 package org.example.models;
 
 public class Comment {
+    private int commentID;
+    private int articleID;
+    private String commentBody;
+
 }
